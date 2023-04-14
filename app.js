7091b6e4e2e7296df332dc2ca3dfe2b91cd80384
alvaro <alvaroceccarelli04@gmail.com>
2023-04-14 12:03:31 -0300

    primer entrega terminada

---
+++ app.js
@@ -0,0 +1,52 @@
+let compra = prompt(`¿que desea comprar de nuestra web?:
+*whey protein
+*creatina
+*pre entreno
+`
+)
+switch (compra){
+    case "whey protein":
+        console.log(`Pedido Confirmado: ${compra}, valor: $15000 + hipotetico recargo por cuotas`)
+        let tarjeta =prompt(`¿con cuantas cuotas quiere pagar?:
+        un pago
+        tres cuotas sin interes
+        seis cuotas con 15% de recargo`)
+        switch (tarjeta){
+            case"un pago":
+            alert("genial!, serian $15000")
+            break
+            case "tres cuotas":
+                alert("prefecto, serian 3 cuotas de $5000 cada una")
+                break
+                case"seis cuotas":
+                alert("gracias,serian 6 cuotas de $2875 cada una")
+                break
+        }
+        break
+        case "creatina":
+            console.log(`Pedido Confirmado: ${compra}, valor: $12000 + hipotetico recargo por cuotas`)
+            let tarjeta1 =prompt(`¿con cuantas cuotas quiere pagar?:
+        un pago
+        tres cuotas sin interes
+        seis cuotas con 15% de recargo`)
+        switch (tarjeta1){
+            case"un pago":
+            alert("genial!, serian $12000")
+            break
+            case "tres cuotas":
+                alert("prefecto, serian 3 cuotas de $4000 cada una")
+                break
+                case"seis cuotas":
+                alert("gracias,serian 6 cuotas de $2300 cada una")
+                break
+        }
+            break
+            case "pre entreno":
+                console.log(`Pedido no Confirmado: ${compra}, motivo: no hay stock`)
+                alert("lo siento no tenemos mas stock, eliga otro producto")
+                break
+                default:
+                    console.log(`ningun producto seleccionado`)
+                    break
+    }
+
